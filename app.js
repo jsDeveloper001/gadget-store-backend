9b0103e5e583fc8f5bee8079fe2607102273de4e
Mujahid <eng.mdmuzahidulislam@gmail.com>
2024-11-20 23:24:13 +0600

    add new user to the database

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const cors = require("cors")
+const jwt = require("jsonwebtoken")
 require("dotenv").config()
 const app = express()
 const port = process.env.port || 8080
@@ -20,6 +21,9 @@ const client = new MongoClient(process.env.uri, {
     }
 });
 
+const GadgetShop = client.db("GadgetShop")
+const UserCollection = GadgetShop.collection("Users")
+const ProductsCollection = GadgetShop.collection("Products")
 const run = async () => {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -28,11 +32,38 @@ const run = async () => {
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
+        // Create New User
+        app.post("/addUser", async (req, res) => {
+            const NewUser = req.body
+            const Query = { email: NewUser.email }
+            const ExisingUser = await UserCollection.findOne(Query)
+            if (ExisingUser) {
+                res.send("Already Existed")
+            }
+            else {
+                const AddedUser = await UserCollection.insertOne(NewUser)
+                res.send(AddedUser)
+            }
+
+        })
+
 
     } catch (error) {
         console.log(error.name, error.message)
     }
 }
+
+
 run()
+
+
+// jwt token POST API for secure APIs
+app.post("/authentication", async (req, res) => {
+    const userEmail = req.body
+
+    // const Token = jwt.sign({data: ""}, "secret Key", {expiresIn: "2d"})
+    const Token = jwt.sign({ userEmail }, process.env.Access_Sign_Key, { expiresIn: "2d" })
+    res.send(Token)
+})
 
 app.listen(port, () => console.log("server running on", port))
